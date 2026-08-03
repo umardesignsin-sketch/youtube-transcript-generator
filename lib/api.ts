055598b893869bde6df07767a7cf6a67b36f3780
umardesignsin-sketch <umardesignsin@gmail.com>
2026-08-03 19:50:48 +0530

    Fix API env variable

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,13 @@
-NEXT_PUBLIC_API_URL=http://127.0.0.1:8000
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+
+export async function getTranscript(url: string) {
+  const res = await fetch(`${API_URL}/transcript`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ url }),
+  });
+
+  return res.json();
+}
