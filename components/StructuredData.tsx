a56aa8464f62f5eb8241dd442e1da33c9dcdf870
umardesignsin-sketch <umardesignsin@gmail.com>
2026-08-04 14:00:20 +0530

    Create StructuredData.tsx

---
+++ components/StructuredData.tsx
@@ -0,0 +1,44 @@
+export default function StructuredData() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Organization",
+        "name": "SaveFromNet",
+        "url": "https://www.savefromnet.fun",
+        "logo": "https://www.savefromnet.fun/logo.png"
+      },
+      {
+        "@type": "WebSite",
+        "name": "SaveFromNet",
+        "url": "https://www.savefromnet.fun",
+        "potentialAction": {
+          "@type": "SearchAction",
+          "target": "https://www.savefromnet.fun/?q={search_term_string}",
+          "query-input": "required name=search_term_string"
+        }
+      },
+      {
+        "@type": "SoftwareApplication",
+        "name": "YouTube Transcript Generator",
+        "applicationCategory": "UtilityApplication",
+        "operatingSystem": "Web",
+        "url": "https://www.savefromnet.fun",
+        "offers": {
+          "@type": "Offer",
+          "price": "0",
+          "priceCurrency": "USD"
+        }
+      }
+    ]
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(jsonLd),
+      }}
+    />
+  );
+}
