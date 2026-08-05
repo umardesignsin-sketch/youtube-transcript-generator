3accd5d6d219251f88af25529d51e272b9b55790
umardesignsin-sketch <umardesignsin@gmail.com>
2026-08-05 15:10:21 +0530

    Add second Monetag ad tag script to <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          src="https://quge5.com/88/tag.min.js"
+          data-zone="267057"
+          async
+          data-cfasync="false"
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         <StructuredData />
         {children}
